cb97452f9ee415987b90cd68a44da82c68b945f1
Agboola Usman <agboolausman58@gmail.com>
2025-11-16 01:07:31 +0100

    final change

--- ai-chat/src/App.jsx
+++ ai-chat/src/App.jsx
@@ -6,24 +6,9 @@ import ChatMessages from "./components/ChatMessages";
 function App() {
   const [chatMessages, setchatMessages] = useState([
     {
-      message: "Hello Chatbot",
-      sender: "user",
-      id: "id1",
-    },
-    {
-      message: "Hello! How can I help you?",
+      message: "Hi! How can I help you?",
       sender: "robot",
       id: "id2",
-    },
-    {
-      message: "Hello! can i get the date?",
-      sender: "user",
-      id: "id3",
-    },
-    {
-      message: "Yes! October 5th",
-      sender: "robot",
-      id: "id4",
     },
   ]);
   return (
